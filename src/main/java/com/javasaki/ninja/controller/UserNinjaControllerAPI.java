6bbf42b19c326feb0c7ce428f1adf8e9e86ffe4c
bagyoda <bagyondaniel@gmail.com>
2019-11-07 13:01:40 +0100

    feat(ctrlExcHndlr): controller exception handler works

--- src/main/java/com/javasaki/ninja/controller/UserNinjaControllerAPI.java
+++ src/main/java/com/javasaki/ninja/controller/UserNinjaControllerAPI.java
@@ -9,11 +9,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 public class UserNinjaControllerAPI {
 
   @PostMapping("/register")
-  public ResponseEntity registration(@RequestBody RegisterDTO registerDTO) {
+  public ResponseEntity registration(@Valid @RequestBody RegisterDTO registerDTO) {
     try {
       return ResponseEntity.status(200).body(new RegisterResponseDTO("ok", "registered successfully"));
     } catch (Exception err) {
