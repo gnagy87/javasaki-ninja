5033906df14dc2efd70009da0dbc40f584069d5e
bberrci <b.berrci@gmail.com>
2019-11-07 16:00:19 +0100

    id in token

--- src/main/java/com/javasaki/ninja/controller/GamePlayControllerAPI.java
+++ src/main/java/com/javasaki/ninja/controller/GamePlayControllerAPI.java
@@ -1,9 +1,23 @@
 package com.javasaki.ninja.controller;
 
+import com.javasaki.ninja.ninja.NinjaHeroService;
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GamePlayControllerAPI {
 
+  private NinjaHeroService ninjaHeroService;
 
+  @Autowired
+  public GamePlayControllerAPI(NinjaHeroService ninjaHeroService) {
+    this.ninjaHeroService = ninjaHeroService;
+  }
+
+//  @PutMapping("/bonus")
+//  public ResponseEntity dailyBonus() {
+//
+//  }
 }
