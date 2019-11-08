6b930eb8348d7310c04c934e95aaa6ac7cad2916
bberrci <b.berrci@gmail.com>
2019-11-08 04:52:10 +0100

    yo

--- src/main/java/com/javasaki/ninja/controller/ControllerM.java
+++ src/main/java/com/javasaki/ninja/controller/ControllerM.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ControllerM {
 
   @GetMapping("/")
-  public String main(){
+  public String main() {
     return "index";
   }
 }
