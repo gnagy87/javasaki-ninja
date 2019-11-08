7279c7d74c965b6b768a265c45d303a27f58197e
Gergely Nagy <gergely.nagy1205@gmail.com>
2019-11-08 04:43:21 +0100

    tables named, index.html

---
+++ src/main/java/com/javasaki/ninja/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.javasaki.ninja.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+  @GetMapping("/index")
+  public String getIndex(){
+    return "index";
+  }
+}
